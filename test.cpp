0a7ed3b4373b9d28c52f43c13a49e1c6c1e5e423
Michał Woś <wosiu@students.mimuw.edu.pl>
2013-12-02 03:47:46 +0100

    test.cpp plik dodany ze schematem testow

---
+++ test.cpp
@@ -0,0 +1,31 @@
+#include <typeinfo>
+#include <cassert>
+#include <iostream>
+#include <vector>
+
+#include "holding.hh"
+
+void operatory_dodawan_odejm_grup_test()
+{
+	//TODO
+}
+
+void operatory_mnozenie_dzielenie_test()
+{
+	//TODO
+}
+
+void operatory_porownan_test()
+{
+	//TODO
+}
+
+int main() {
+
+	operatory_dodawan_odejm_grup_test();
+	operatory_mnozenie_dzielenie_test();
+	operatory_porownan_test();
+	//itd..
+
+	cout << "OK :)\n";
+}
